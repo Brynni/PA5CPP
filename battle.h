8e2fa7b32ef819d48d6178d039377b0b0cfeb15e
Vésteinn Bjarnason <vesteinnbjarnason@gmail.com>
2021-04-06 10:12:46 +0000

    battle.h

--- battle.h
+++ battle.h
@@ -4,10 +4,13 @@
 #include <random>
 #include <chrono>
 #include "being.h"
+#include "investigator.h"
 using namespace std;
 
 
 Encounter getRandomEncounter(int difficultyLevel, vector<Encounter>AllEncounters);
 vector<Encounter> shuffleVector(vector<Encounter>AllEncounters);
 Encounter randomEncounter(vector<Encounter>filteredVector, int difficultyLevel);
+void battleEnv(Encounter randomEnc, vector<Investigator> characters);
+int * generateInitiveOrder(Encounter randomEnc, vector<Investigator> characters );
 
