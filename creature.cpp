11181d35d9257be43ba87fbfa7c31baa6e88e88a
Vésteinn Bjarnason <vesteinnbjarnason@gmail.com>
2021-04-08 11:08:00 +0000

    pushed

--- creature.cpp
+++ creature.cpp
@@ -314,7 +314,7 @@ Creature & selectCreature(vector<Creature> &creatures)
     return creatures[selection -1];
 }
 
-Creature & selectCreatureWithType(const vector<Creature>&creatures, const string &type)
+Creature  selectCreatureWithType(const vector<Creature>&creatures, const string &type)
 {
     vector <Creature> tempCreatures = seeAllCreaturesOfType(creatures, type);
     int selection;
